bfc8c2944b2346c5c83b10832deb76d2d2773b1a
Kushagra Kumar <167355875+KushagraKumar04@users.noreply.github.com>
2025-12-20 15:06:58 +0530

    Update server.js

--- js/server.js
+++ js/server.js
@@ -75,3 +75,8 @@ app.get('/api/random-track', async (req, res) => {
   });
 
   module.exports = app;
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
